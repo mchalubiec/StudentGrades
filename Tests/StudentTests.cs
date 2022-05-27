e5e998d930fcb4bcdf6cb2acaffb4414d102064f
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-05-27 23:09:46 +0200

    added student test

---
+++ Tests/StudentTests.cs
@@ -0,0 +1,25 @@
+using StudentGrades;
+using System;
+using Xunit;
+
+namespace Tests
+{
+    public class StudentTests
+    {
+        [Fact]
+        public void Test1()
+        {
+            //arrange
+            var student = new Student("Micha³");
+            student.AddGrade(87.2);
+            student.AddGrade(90.5);
+            student.AddGrade(77.3);
+            //act
+            var result = student.GetStatistics();
+            //assert
+            Assert.Equal(90.5, result.High);
+            Assert.Equal(77.3, result.Low);
+            Assert.Equal(85, result.Average);
+        }
+    }
+}
