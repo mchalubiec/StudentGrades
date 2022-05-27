42d896786f919fe4c2faaf2c0aeece65570c7954
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-05-27 22:32:19 +0200

    added test body

--- StudentGrades/Program.cs
+++ StudentGrades/Program.cs
@@ -6,7 +6,11 @@ namespace StudentGrades
     {
         static void Main(string[] args)
         {
-            
+            var employee = new Student("Michał");
+            employee.AddGrade(4.5);
+            employee.AddGrade(4.0);
+            employee.AddGrade(5.0);
+            employee.GetStatistics();
         }
     }
 }
