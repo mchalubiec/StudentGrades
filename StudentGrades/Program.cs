6d144a1d290314fbc78ec649ac52a75713c9bb6b
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-05-27 22:25:01 +0200

    Add project files.

---
+++ StudentGrades/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace StudentGrades
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
