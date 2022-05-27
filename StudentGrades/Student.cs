dc227ae736615405caa9e9ef546ba9dc7b459725
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-05-27 22:26:58 +0200

    added student class

---
+++ StudentGrades/Student.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentGrades
+{
+    public class Student
+    {
+        private string name;
+        private List<double> grades = new List<double>();
+        public Student(string name)
+        {
+            this.name = name;
+        }
+    }
+}
