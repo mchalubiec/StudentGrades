feadf64b22adaca847d74e3624b51aa95cf406e9
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-02 21:00:47 +0200

    added nameobject class

---
+++ StudentGrades/NameObject.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentGrades
+{
+    public class NameObject
+    {
+        private string firstName;
+        public NameObject(string firstName)
+        {
+            this.firstName = firstName;
+        }
+        public string FirstName
+        {
+            get { return firstName; }
+            set
+            {
+                if (!string.IsNullOrWhiteSpace(value))
+                {
+                    this.firstName = value;
+                }
+            }
+        }
+    }
+}
