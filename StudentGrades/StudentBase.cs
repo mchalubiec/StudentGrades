db2a5b9ab140fb7fa19cd51c508ce1f8c0d209cd
Michał Chałubiec <michal.chalubiec@outlook.com>
2022-06-02 21:00:28 +0200

    added studentbase class

---
+++ StudentGrades/StudentBase.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace StudentGrades
+{
+    public abstract class StudentBase : NameObject
+    {
+        public StudentBase(string firstName) : base(firstName)
+        {
+
+        }
+        public abstract void AddGrade(double grade);
+    }
+}
